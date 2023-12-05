d9be950fc33514da3d970a28571b1d4117974606
Pedro <pwbarbosa@gmail.com>
2023-12-04 19:48:01 -0500

    made some adjustments. learned that pokemon go forward from 10001. So had the code move forward in the catch.

--- main.js
+++ main.js
@@ -65,11 +65,14 @@ $(document).ready(function() {
             }
         }).catch(function(e){
             console.error(e);
-            console.log("Looks like we couldn't grab anymore Pokemon!")
-            console.log(pokeCache);
+            console.log("Looks like we couldn't grab anymore here! Let's skip ahead");
+            nextNum = 10001;
+            getNextPokemon(nextNum);
             //maybe have the system try to run getNextPokemon() from one more than last known index 5-10 more times. 
             //We could also save the final index in cookies, if not the whole array, to test to see if it is likely that we have all
             //the pokemon possible
+        }).catch(function(e){
+            console.error(e);
         });
     }
 
